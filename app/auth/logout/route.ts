00ae5398d128320b7eb1e92a7c8a3b1cbfe8fd3c
Fuckyou <kashifkhan0336@gmail.com>
2025-01-16 21:36:41 +0500

    zithadel integrated

---
+++ app/auth/logout/route.ts
@@ -0,0 +1,15 @@
+import { defaultSession, getClientConfig, getSession, clientConfig } from '@/lib'
+import * as client from 'openid-client'
+
+export async function GET() {
+  const session = await getSession()
+  const openIdClientConfig = await getClientConfig()
+  const endSessionUrl = client.buildEndSessionUrl(openIdClientConfig)
+  session.isLoggedIn = defaultSession.isLoggedIn
+  session.access_token = defaultSession.access_token
+  session.userInfo = defaultSession.userInfo
+  session.code_verifier = defaultSession.code_verifier
+  session.state = defaultSession.state
+  await session.save()
+  return Response.redirect(endSessionUrl.href)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Login from '@/components/Login'
+import { getSession } from '@/lib'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession()
   return (
-    <div className={styles.page}>
-      <h1>Kashif</h1>
-      <main className={styles.main}>
-
-      </main>
-    </div>
-  );
+    <main className="flex min-h-[100dvh] items-center justify-center bg-gray-100 px-4 dark:bg-gray-900">
+      <div className="w-full max-w-md space-y-4 text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-50">
+          Welcome back
+        </h1>
+        <p className="text-gray-500 dark:text-gray-400">Sign in to your account to continue.</p>
+        <div>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
+        <Login />
+      </div>
+    </main>
+  )
 }
